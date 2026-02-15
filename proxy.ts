5964b17b46596a7e63e141bf2628f5fbdcb517d5
Azer Kazimov <azer.kazimov@yahoo.com>
2026-02-15 12:38:32 +0400

    fix proxy

--- proxy.ts
+++ proxy.ts
@@ -1,14 +1,14 @@
 import { getToken } from "next-auth/jwt";
 import { NextRequest, NextResponse } from "next/server";
 
-const authPages = [
-    "/auth/signin",
-]
+const authPages = ["/auth/signin"]
+const protectedPages = ["/dashboard"]
 
 export default async function proxy(req: NextRequest) {
     const { pathname } = req.nextUrl;
 
     const isAuthPage = authPages.some(page => pathname.startsWith(page));
+    const isProtectedPage = protectedPages.some(page => pathname.startsWith(page));
 
     const session = await getToken({
         req,
@@ -16,6 +16,22 @@ export default async function proxy(req: NextRequest) {
     })
 
     if (isAuthPage && session) {                         // email sign in verification
+        return NextResponse.redirect(new URL("/", req.url));
+    }
+
+    if (session?.email !== "azer.kazimov@yahoo.com") {
+        return NextResponse.redirect(new URL("/", req.url));
+    }
+
+    if (session?.email && isAuthPage){
+        return NextResponse.redirect(new URL("/dashboard", req.url));
+    }
+
+    if (!session?.email && isProtectedPage){
+        return NextResponse.redirect(new URL("/auth/signin", req.url));
+    }
+
+    if (session?.email !== "azer.kazimov@yahoo.com" && isProtectedPage){
         return NextResponse.redirect(new URL("/", req.url));
     }
 
