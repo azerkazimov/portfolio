a9cf17718138e7d331ba02b79ecaaa51d79e0f98
Azer Kazimov <azer.kazimov@yahoo.com>
2026-02-22 12:54:28 +0400

    feat: implement service data fetching in Dashboard component and update .gitignore for env files

---
+++ app/[locale]/portfolio/page.tsx
@@ -0,0 +1,26 @@
+import Counter from "@/components/pages/counter";
+import { Service } from "@/types/services";
+
+
+export default async function Portfolio() {
+
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/services`);
+    const services = await response.json();
+
+
+
+    return (
+        <div className="container mx-auto">
+            <h1>Portfolio</h1>
+            <div className="my-6 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                {services.map((service: Service) => (
+                    <div key={service._id} className="border p-4 rounded-md">
+                        <h2>{service.name.az}</h2>
+                        <p>{service.description.az}</p>
+                    </div>
+                ))}
+            </div>
+            <Counter />
+        </div>
+    )
+}
---
+++ types/services.d.ts
@@ -0,0 +1,13 @@
+export interface Service {
+    _id: string;
+    name: {
+        az: string;
+        en: string;
+        ru: string;
+    };
+    description: {
+        az: string;
+        en: string;
+        ru: string;
+    };
+}
